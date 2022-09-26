259e9ea2f3a0904070be5ec668edfff2523239e3
panghuuuu <82875686+panghuuuu@users.noreply.github.com>
2022-09-26 13:09:06 +0800

    ringbuffer-edited

--- guitar_files/ringbuffer.py
+++ guitar_files/ringbuffer.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python3
-
 class RingBuffer:
     def __init__(self, capacity: int):
         '''
         Create an empty ring buffer, with given max capacity
         '''
         self.capacity = capacity
-        self.buffer = []
+        self.buffer = list()
+        self.front = self.rear = 0
 
     def size(self) -> int:
         '''
@@ -30,21 +29,36 @@ class RingBuffer:
         '''
         Add item `x` to the end
         '''
-        self.buffer.append(x)
+        if self.is_full == True:
+            RingBufferFull
+        elif self.rear == self.capacity:
+            self.rear = 0
+        else:
+            self.buffer.append(x)
+            self.rear += 1
 
     def dequeue(self) -> float:
         '''
         Return and remove item from the front
         '''
-        return self.buffer.remove(self.buffer[0])
-
+        if self.is_empty == True:
+            RingBufferEmpty
+        
+        elif self.front == self.capacity:
+            self.front = 0
+        else:
+            x = self.buffer[self.front]
+            self.front += 1
+            return x 
     def peek(self) -> float:
         '''
         Return (but do not delete) item from the front
         '''
-        return self.buffer[0]
-
-
+        return self.buffer[self.front]
+    
+    def printBuffer(self):
+        print(self.buffer)
+        
 class RingBufferFull(Exception):
     pass
 
