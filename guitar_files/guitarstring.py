754f151a812a493361ad81455126e6bc691ce082
panghuuuu <82875686+panghuuuu@users.noreply.github.com>
2022-10-02 22:17:58 +0800

    guitar-string

--- guitar_files/guitarstring.py
+++ guitar_files/guitarstring.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-
+from ringbuffer import RingBuffer
 class GuitarString:
     def __init__(self, frequency: float):
         '''
         Create a guitar string of the given frequency, using a sampling rate of 24000 Hz
         '''
-        # TO-DO: implement this
-        #         
+        N = 24000/frequency
+        newBuffer = RingBuffer(int(N))
+        for i in range(N):
+            newBuffer.enqueue(0)
+                 
     def make_from_array(self, init: list[int]):
         '''
         Create a guitar string whose size and initial values are given by the array `init`
