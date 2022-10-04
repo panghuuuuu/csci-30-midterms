65d12d0f5e89406c14a18048c33c8127aac6f40f
jame6260 <julia.espera@obf.ateneo.edu>
2022-10-04 14:54:33 +0800

    buffer and guitar 2

--- guitar_files/guitar.py
+++ guitar_files/guitar.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     keyboard = ["q","2","w","e","4","r","5","t","y","7","u","8","i","9","o","p"]
     string = list()
     for i in range(16):
-        x = 440*pow(1.059463,(i-12))
+        x = 440*(1.059463**(i-12))
         string.append(GuitarString(x))
     sample = 0
 
@@ -28,11 +28,11 @@ if __name__ == '__main__':
                     x = keyboard.index(key)
                     string[x].pluck()
                 else:
-                    print('no')
+                    pass
 
-        for i in range(len(string)):
+        for i in range(16):
             sample += string[i].sample()
         play_sample(sample)
 
-        for i in range(len(string)):
+        for i in range(16):
             string[i].tick()
